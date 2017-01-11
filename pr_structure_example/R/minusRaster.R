8a18cd44ab2ef10ca3e044b2894b0e5999192265
jaspersiebring <j.siebring92@gmail.com>
2017-01-11 11:46:08 +0100

    Fixed minusRaster #Description

--- pr_structure_example/R/minusRaster.R
+++ pr_structure_example/R/minusRaster.R
@@ -25,6 +25,3 @@ r2[] <- (1:ncell(r2)) / 10
 r3 <- minusRaster(r, r2) 
 ## Now performs the calculation and plots the resulting RasterLayer
 r4 <- minusRaster(r, r2, plot=TRUE) 
-
-a = raster(nrow=50, ncol=50)
-a[] = floor(rnorm())
